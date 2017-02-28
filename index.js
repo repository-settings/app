44e017c2cda1c4a2c22bf049caa748e5c4d8cc02
Brandon Keepers <bkeepers@github.com>
2017-02-28 17:59:55 -0600

    Replace integration code with github-integration

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const fs = require('fs');
+const createIntegration = require('github-integration');
 const Server = require('./lib/server.js');
 const log = require('./lib/log');
+
+const integration = createIntegration({
+  id: process.env.INTEGRATION_ID,
+  cert: process.env.PRIVATE_KEY || fs.readFileSync('private-key.pem'),
+  debug: log.level() <= 10
+});
 
 // Show trace for any unhandled rejections
 process.on('unhandledRejection', reason => {
   log.error(reason);
 });
 
-new Server().start();
+new Server(integration).start();
