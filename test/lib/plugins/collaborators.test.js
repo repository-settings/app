e89f104f87abf0f9cf3ce3dd1203327bddafd71e
John Sloan <jwsloan@gmail.com>
2018-03-11 18:15:00 -0500

    Extract collaborators tests

---
+++ test/lib/plugins/collaborators.test.js
@@ -0,0 +1,63 @@
+const Collaborators = require('../../../lib/plugins/collaborators')
+
+describe('Collaborators', () => {
+  let github
+
+  function configure (config) {
+    return new Collaborators(github, {owner: 'bkeepers', repo: 'test'}, config)
+  }
+
+  beforeEach(() => {
+    github = {
+      repos: {
+        getCollaborators: jest.fn().mockImplementation(() => Promise.resolve([])),
+        removeCollaborator: jest.fn().mockImplementation(() => Promise.resolve()),
+        addCollaborator: jest.fn().mockImplementation(() => Promise.resolve())
+      }
+    }
+  })
+
+  describe('sync', () => {
+    it('syncs collaborators', () => {
+      const plugin = configure([
+        {username: 'bkeepers', permission: 'admin'},
+        {username: 'added-user', permission: 'push'},
+        {username: 'updated-permission', permission: 'push'},
+        {username: 'DIFFERENTcase', permission: 'push'}
+      ])
+
+      github.repos.getCollaborators.mockReturnValueOnce(Promise.resolve({data: [
+        {login: 'bkeepers', permissions: {admin: true, push: true, pull: true}},
+        {login: 'updated-permission', permissions: {admin: false, push: false, pull: true}},
+        {login: 'removed-user', permissions: {admin: false, push: true, pull: true}},
+        {login: 'differentCase', permissions: {admin: false, push: true, pull: true}}
+      ]}))
+
+      return plugin.sync().then(() => {
+        expect(github.repos.addCollaborator).toHaveBeenCalledWith({
+          owner: 'bkeepers',
+          repo: 'test',
+          username: 'added-user',
+          permission: 'push'
+        })
+
+        expect(github.repos.addCollaborator).toHaveBeenCalledWith({
+          owner: 'bkeepers',
+          repo: 'test',
+          username: 'updated-permission',
+          permission: 'push'
+        })
+
+        expect(github.repos.addCollaborator).toHaveBeenCalledTimes(2)
+
+        expect(github.repos.removeCollaborator).toHaveBeenCalledWith({
+          owner: 'bkeepers',
+          repo: 'test',
+          username: 'removed-user'
+        })
+
+        expect(github.repos.removeCollaborator).toHaveBeenCalledTimes(1)
+      })
+    })
+  })
+})
--- test/settings.test.js
+++ test/settings.test.js
@@ -10,57 +10,6 @@ describe('Settings', () => {
   }
 
   describe('update', () => {
-    it('syncs collaborators', () => {
-      github.repos.getCollaborators = jest.fn().mockImplementation(() => Promise.resolve([]))
-      github.repos.removeCollaborator = jest.fn().mockImplementation(() => Promise.resolve())
-      github.repos.addCollaborator = jest.fn().mockImplementation(() => Promise.resolve())
-
-      github.repos.getCollaborators.mockReturnValueOnce(Promise.resolve({data: [
-        {login: 'bkeepers', permissions: {admin: true, push: true, pull: true}},
-        {login: 'updated-permission', permissions: {admin: false, push: false, pull: true}},
-        {login: 'removed-user', permissions: {admin: false, push: true, pull: true}},
-        {login: 'differentCase', permissions: {admin: false, push: true, pull: true}}
-      ]}))
-
-      const config = configure(`
-        collaborators:
-          - username: bkeepers
-            permission: admin
-          - username: added-user
-            permission: push
-          - username: updated-permission
-            permission: push
-          - username: DIFFERENTcase
-            permission: push
-      `)
-
-      return config.update().then(() => {
-        expect(github.repos.addCollaborator).toHaveBeenCalledWith({
-          owner: 'bkeepers',
-          repo: 'test',
-          username: 'added-user',
-          permission: 'push'
-        })
-
-        expect(github.repos.addCollaborator).toHaveBeenCalledWith({
-          owner: 'bkeepers',
-          repo: 'test',
-          username: 'updated-permission',
-          permission: 'push'
-        })
-
-        expect(github.repos.addCollaborator).toHaveBeenCalledTimes(2)
-
-        expect(github.repos.removeCollaborator).toHaveBeenCalledWith({
-          owner: 'bkeepers',
-          repo: 'test',
-          username: 'removed-user'
-        })
-
-        expect(github.repos.removeCollaborator).toHaveBeenCalledTimes(1)
-      })
-    })
-
     it('syncs teams', () => {
       github.orgs = {}
       github.orgs.deleteTeamRepo = jest.fn().mockImplementation(() => Promise.resolve())
