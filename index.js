fc15878696ae78b1a997faa7b675c625586a69f2
knrt10 <tripathi.kautilya@gmail.com>
2018-03-18 02:39:37 +0530

    changes

--- index.js
+++ index.js
@@ -15,23 +15,21 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     }
   }
 
-  robot.on('installation_repositories.added',installSync)
+  robot.on('installation_repositories.added', installSync)
 
-    async function installSync(context){
-      const payload =  context.payload
-      // getting first repo id that is added
-      const repoAddedId =  payload.repositories_added[0].id;
-
-      // getting repo information
-      const result =  await context.github.repos.getById({id: repoAddedId})
-
-      const owner = result.data.owner.login
-      const repoName = result.data.name
-      // As context.repo() was undefined so had to convert it into object
-      const repo = {
-        owner : owner,
-        repo : repoName
-      }
-      return Settings.sync(context.github, repo)
+  async function installSync (context) {
+    const payload = context.payload
+    // getting first repo id that is added
+    const repoAddedId = payload.repositories_added[0].id
+    // getting repo information
+    const result = await context.github.repos.getById({id: repoAddedId})
+    const owner = result.data.owner.login
+    const repoName = result.data.name
+    // As context.repo() was undefined so had to convert it into object
+    const repo = {
+      owner : owner,
+      repo : repoName
     }
+      return Settings.sync(context.github, repo)
+  }
 }
