b4d9a01cd132c810be84c01eeced202cc805ef79
Aryan Arora <aryanarora278@gmail.com>
2018-03-18 23:10:25 +0530

    test handled

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
 
     if (defaultBranch && settingsModified) {
       return Settings.sync(context.github, context.repo())
-    }
-    else if (!defaultBranch && settingsModified) {
-      let settingChangeCommits = payload.commits.filter(commit => {  //get all commits that change settings.yml
-        return commit.added.includes(Settings.FILE_NAME) || 
+    } else if (!defaultBranch && settingsModified) {
+      let settingChangeCommits = payload.commits.filter(commit => { // get all commits that change settings.yml
+        return commit.added.includes(Settings.FILE_NAME) ||
         commit.modified.includes(Settings.FILE_NAME)
       })
-      
-      settingChangeCommits.forEach(function(commit, index, array) { //create status for each commit that changed settings.yml
-        context.github.repos.createStatus({
-          owner: payload.repository.owner.login,
-          repo: payload.repository.name,
-          sha: commit.id,
-          state: 'error'
-        })
+      settingChangeCommits.forEach(async function (commit, index, array) { // create status for each commit that changed settings.yml
+        try {
+          await context.github.repos.createStatus({
+            owner: payload.repository.owner.login,
+            repo: payload.repository.name,
+            sha: commit.id,
+            state: 'error'
+          })
+        } catch (err) {
+          console.log(new Error(err))
+        }
       })
     }
   }
