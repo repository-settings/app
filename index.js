e562583ab52bd51199768057a8e219ab18288a46
knrt10 <tripathi.kautilya@gmail.com>
2018-03-19 15:30:13 +0530

    added enhancement Validate configuration and update the status

--- index.js
+++ index.js
@@ -12,6 +12,35 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
 
     if (defaultBranch && settingsModified) {
       return Settings.sync(context.github, context.repo())
+    } else if (!defaultBranch && settingsModified) {
+      const fileModified = payload.commits.filter(commit => {
+        return commit.added.includes(Settings.FILE_NAME) ||
+          commit.modified.includes(Settings.FILE_NAME)
+      })
+      fileModified.forEach(async function (commit) {
+        if (fileModified) {
+          const ref = payload.ref.slice(11)
+          const path = '.github/settings.yml'
+          const owner = payload.repository.owner.login
+          const repo = payload.repository.name
+          const repoInfo = await context.github.repos.getContent({owner: owner, repo: repo, path: path, ref: ref})
+          const content = repoInfo.data.content
+          const sha = payload.commits[0].id
+          const base64 = require('./encode')
+          const result = base64.decode(content)
+          const yaml = require('js-yaml')
+          try {
+            var doc = yaml.safeLoad(result)
+            if (doc) {
+              await context.github.repos.createStatus({owner: owner, repo: repo, sha: sha, state: 'success', description: 'Correct settings.yml file'})
+            } else {
+              context.log('Sorry')
+            }
+          } catch (e) {
+            await context.github.repos.createStatus({owner: owner, repo: repo, sha: sha, state: 'error', description: 'Correct settings.yml file'})
+          }
+        }
+      })
     }
   }
 }
