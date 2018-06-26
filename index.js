365aeafe33b72bef07a5d435d31a98e361dcf4b5
Philip Holleran <pholleran@github.com>
2018-06-25 21:33:54 -0500

    implement probot-config

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const getConfig = require('probot-config')
+
 module.exports = (robot, _, Settings = require('./lib/settings')) => {
   robot.on('push', async context => {
     const payload = context.payload
     const defaultBranch = payload.ref === 'refs/heads/' + payload.repository.default_branch
+
+    const config = getConfig(context, 'settings.yml')
+    console.log(config)
 
     const settingsModified = payload.commits.find(commit => {
       return commit.added.includes(Settings.FILE_NAME) ||
@@ -9,7 +14,7 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     })
 
     if (defaultBranch && settingsModified) {
-      return Settings.sync(context.github, context.repo())
+      return Settings.sync(context.github, context.repo(), config)
     }
   })
 }
