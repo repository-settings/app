e6cd6df839df3f092a8d6c857c78534c22a34a41
Matt Travi <programmer@travi.org>
2019-09-24 22:09:33 -0500

    Remove probot-config
    since it is now included in core probot

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const getConfig = require('probot-config')
 const mergeArrayByName = require('./lib/mergeArrayByName')
 
 module.exports = (robot, _, Settings = require('./lib/settings')) => {
@@ -6,7 +5,7 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     const payload = context.payload
     const defaultBranch = payload.ref === 'refs/heads/' + payload.repository.default_branch
 
-    const config = await getConfig(context, 'settings.yml', {}, { arrayMerge: mergeArrayByName })
+    const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
 
     const settingsModified = payload.commits.find(commit => {
       return commit.added.includes(Settings.FILE_NAME) ||
