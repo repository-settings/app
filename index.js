8ddcc3b59d715c68bf89a720a461b4f86363cb99
Leopoldo Fu <lfu@ebsco.com>
2019-10-09 17:22:43 -0400

    Working successfully: adds a team in codeowners as admin to repository. Extremely limited parsing of code owners file.

--- index.js
+++ index.js
@@ -12,8 +12,44 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
         commit.modified.includes(Settings.FILE_NAME)
     })
 
-    if (defaultBranch && settingsModified) {
-      return Settings.sync(context.github, context.repo(), config)
-    }
+    console.log('-- -- before get code owners')
+    return context.github.repos.getContents({
+      owner: context.repo().owner,
+      repo: context.repo().repo,
+      path: 'CODEOWNERS'
+    })
+      .then(result => {
+        // content will be base64 encoded
+        const content = Buffer.from(result.data.content, 'base64').toString()
+        const lines = content.split('\n')
+        const teamWithOrg = lines[0].split(' ')[1]
+        const org = teamWithOrg.split('/')[0].split('@')[1]
+        const teamName = teamWithOrg.split('/')[1]
+        
+        console.log('-- -- codeowners content: ')
+        console.log(content)
+        
+        return context.github.teams.getByName({
+          org: org,
+          team_slug: teamName
+        })
+      })
+      .then(result => {
+        // console.log(`---- team: ${JSON.stringify(team)}`)
+        return {
+          team_id: result.data.id,
+          owner: context.repo().owner,
+          repo: context.repo().repo,
+          permission: 'push'
+        }
+      })
+      .then(params => {
+        return context.github.teams.addOrUpdateRepo(params)
+      })
+
+    // if (defaultBranch && settingsModified) {
+    //   return Settings.sync(context.github, context.repo(), config)
+    // }
   })
 }
+
