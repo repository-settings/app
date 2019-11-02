d4648e9b032a5777bad17620a8dfeb582b2a1c3c
Martijn Vegter <martijn@martijnvegter.com>
2019-11-02 17:35:13 +0100

    Simplified logic and saved some resources

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const mergeArrayByName = require('./lib/mergeArrayByName')
 
 module.exports = (robot, _, Settings = require('./lib/settings')) => {
   robot.on('push', async context => {
-    const payload = context.payload
-    const defaultBranch = payload.ref === 'refs/heads/' + payload.repository.default_branch
+    const { payload } = context
+    const { repository } = payload
 
-    const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
+    const defaultBranch = payload.ref === 'refs/heads/' + repository.default_branch
+    if (!defaultBranch) {
+      robot.log.debug('Not the default branch, nothing to see here!')
+      return
+    }
 
     const settingsModified = payload.commits.find(commit => {
       return commit.added.includes(Settings.FILE_NAME) ||
         commit.modified.includes(Settings.FILE_NAME)
     })
 
-    if (defaultBranch && settingsModified) {
+    if (settingsModified) {
+      const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
       return Settings.sync(context.github, context.repo(), config)
     }
   })
