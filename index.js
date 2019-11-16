d18b2a7f11e265c084eece4375c1235339f6bd01
Martijn Vegter <martijn@martijnvegter.com>
2019-11-16 11:53:44 +0100

    Sync settings after App installation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const yaml = require('js-yaml')
 const mergeArrayByName = require('./lib/mergeArrayByName')
 
 module.exports = (robot, _, Settings = require('./lib/settings')) => {
@@ -5,6 +6,42 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
     return Settings.sync(context.github, repo, config)
   }
+
+  robot.on('installation', async context => {
+    const { github, payload } = context
+    const { action, repositories, installation } = payload
+    const { account } = installation
+    const { login: repositoryOwner } = account
+
+    if (action === 'deleted') {
+      robot.log.debug('Integration deleted, returning...')
+      return
+    }
+
+    await Promise.all(repositories.map(async (repository) => {
+      const { name: repositoryName } = repository
+
+      const repo = {
+        owner: repositoryOwner,
+        repo: repositoryName
+      }
+
+      var res
+      try {
+        res = await github.repos.getContents(Object.assign(repo, { path: Settings.FILE_NAME }))
+      } catch (error) {
+        if (error.status !== 404) {
+          robot.log.warn(`Unknown error ${error.status} occurred when fetching '${Settings.FILE_NAME}' in '${repositoryOwner}/${repositoryName}', returning...`)
+        } else {
+          robot.log.debug(`File '${Settings.FILE_NAME}' not found in '${repositoryOwner}/${repositoryName}', returning...`)
+        }
+        return
+      }
+
+      const config = yaml.safeLoad(Buffer.from(res.data.content, 'base64').toString()) || {}
+      return Settings.sync(context.github, repo, config)
+    }))
+  })
 
   robot.on('push', async context => {
     const { payload } = context
