3ec20a913dddd887a2c09bbf6ae5d1c0a87179df
James Tanner <jftanner@us.ibm.com>
2022-05-05 12:08:42 -0400

    Add specific warnings for GHE_HOST mismatch

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ import SettingsApp from './lib/settings'
  */
 export default (robot, _, Settings = SettingsApp) => {
   async function syncSettings (context, repo = context.repo()) {
+    const { host } = context
+    const { GHE_HOST } = process.env
+    if (host === 'github.com' && GHE_HOST) {
+      robot.log.warn('Webhook source is public github, but GHE_HOST is set. Settings may not be read correctly.')
+    }
+    else if (host !== GHE_HOST) {
+      robot.log.warn('Webhook source does not match GHE_HOST. Settings may not be read correctly.')
+    }
+
     const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
     if (!config || !Object.keys(config).length) {
-      robot.log.debug(`Unable to read config from '${Settings.FILE_NAME}', returning...`)
+      robot.log.error(`Unable to read config from '${Settings.FILE_NAME}'`)
       return
     }
     const results = Settings.sync(context.octokit, repo, config)
