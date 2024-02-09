f039855edac2c0b0339d58c7e2819f676030b731
Matt Travi <programmer@travi.org>
2024-02-09 15:37:01 -0600

    test(msw): reset handlers between tests

--- test/integration/features/step_definitions/github-api-steps.mjs
+++ test/integration/features/step_definitions/github-api-steps.mjs
@@ -1,15 +1,20 @@
-import { AfterAll, Before, BeforeAll } from '@cucumber/cucumber'
+import { After, AfterAll, Before, BeforeAll } from '@cucumber/cucumber'
 import { setupServer } from 'msw/node'
 import any from '@travi/any'
 
 const server = setupServer()
 export const githubToken = any.word()
+
 BeforeAll(async function () {
   server.listen()
 })
 
 Before(function () {
   this.server = server
+})
+
+After(function () {
+  server.resetHandlers()
 })
 
 AfterAll(function () {
