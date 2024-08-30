5b9a0e74e0b96555dd104897b9bf08a028df27e7
Matt Travi <programmer@travi.org>
2024-08-30 16:18:55 -0500

    test(integration): defined rulesets fixtures to a failing state
    for #732

---
+++ test/integration/features/step_definitions/rulesets-steps.mjs
@@ -0,0 +1,40 @@
+import { dump } from 'js-yaml'
+import { StatusCodes } from 'http-status-codes'
+
+import { Given, Then } from '@cucumber/cucumber'
+import assert from 'node:assert'
+import { http, HttpResponse } from 'msw'
+
+import { repository } from './common-steps.mjs'
+import settings from '../../../../lib/settings.js'
+
+Given('no rulesets are defined for the repository', async function () {
+  this.server.use(
+    http.get(`https://api.github.com/repos/${repository.owner.name}/${repository.name}/rulesets`, ({ request }) =>
+      HttpResponse.json([])
+    )
+  )
+})
+
+Given('a ruleset is defined in the config', async function () {
+  this.server.use(
+    http.get(
+      `https://api.github.com/repos/${repository.owner.name}/${repository.name}/contents/${encodeURIComponent(
+        settings.FILE_NAME
+      )}`,
+      ({ request }) => HttpResponse.arrayBuffer(Buffer.from(dump({ rulesets: [{}] })))
+    ),
+    http.post(
+      `https://api.github.com/repos/${repository.owner.name}/${repository.name}/rulesets`,
+      async ({ request }) => {
+        this.createdRuleset = await request.json()
+
+        return new HttpResponse(null, { status: StatusCodes.CREATED })
+      }
+    )
+  )
+})
+
+Then('the ruleset is enabled for the repository', async function () {
+  assert.deepEqual(this.createdRuleset, {})
+})
